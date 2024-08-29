2468d08e2b361f27404f0062a5ae5accffe2ed0f
vinifen <ferreiranivicius@example.com>
2024-08-29 20:09:01 -0300

    home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,34 +2,40 @@ import { Link } from 'react-router-dom';
 
 export default function Home(){
   return ( 
-    <>
-    <header>
-      <nav>
-        <Link to="/"><img src="../images/archive-icon.png" alt="Logo 1" className='h-10 w-10 p-1'/></Link>
-        <Link to="/login"><img src="../images/profile-icon.png" alt="Logo 2" className='h-10 w-10 p-1'/></Link>
-      </nav>
-    </header>
-    <main>
-      <section>
-        <div>
+    <div className='min-h-screen grid grid-rows-[auto_1fr_auto]'>
+      <header>
+        <nav className='flex justify-between mt-6 mx-6'>
+          <Link to="/"><img className='h-12 md:h-16 p-1 shadow-xl' src="../images/archive-icon.png" alt="archive"/></Link>
+          <Link to="/login"><img className='h-12 md:h-16 p-1 shadow-xl' src="../images/profile-icon.png" alt="login"/></Link>
+        </nav>
+      </header>
+      <main className='flex flex-col items-center justify-center'>
+        <section className='flex flex-col md:flex-row items-center justify-center md:mb-14'>
+          <img className='h-24 md:h-28 mr-4' src="../images/pixel_post-logo.png" alt="logo" /> 
+          <h1 className='text-2xl md:text-3xl font-extrabold mt-5 md:mt-0'>PIXEL POST</h1>
+        </section>
+        <Link to="/canvas" className='text-xl md:text-2xl text-center font-semibold py-6 sm:px-20 md:px-28 lg:px-36 w-9/12 sm:w-auto mt-6 mb-3 md:mb-8 bg-primary-button shadow-xl'>start create</Link>
+        <section className='mt-8 md:mt-16 md:mb-20'>
+          <div className='grid grid-cols-2 sm:grid-cols-4 gap-4 md:gap-6 p-4'>
+            <img className='h-24 md:h-28  p-1 shadow-xl' src="../images/archive-icon.png" alt="archive"/>
+            <img className='h-24 md:h-28 ' src="../images/pixel_post-logo.png" alt="logo" />
+            <img className='h-24 md:h-28  p-1 shadow-xl' src="../images/profile-icon.png" alt="login"/>
+            <img className='h-24 md:h-28  p-1 shadow-xl' src="../images/archive-icon.png" alt="archive"/>
+            {/*<img className='h-24' src="../images/pixel_post-logo.png" alt="logo" />
+            <img className='h-24 p-1 shadow-xl' src="../images/profile-icon.png" alt="login"/>
+            <img className='h-24 p-1 shadow-xl' src="../images/archive-icon.png" alt="archive"/>
+            <img className='h-24' src="../images/pixel_post-logo.png" alt="logo" />*/}
+            {/*images*/}
+          </div>
+        </section>
+      </main>
+      <footer className=' pt-10 md:pt-20'>
+        <div className='text-center'>
+          <cite className='text-secundary-color-text'>vinifen</cite>
           <img src="" alt="" />
-          <h1>Pixel Post</h1>
         </div>
-        <Link to="/canvas">START CREATE</Link>
-      </section>
-      <section>
-        <div>
-          {/*images*/}
-        </div>
-      </section>
-    </main>
-    <footer>
-      <p>
-        <cite>vinifen</cite>
-        <img src="" alt="" />
-      </p>
-    </footer>
-    </>
+      </footer>
+    </div>
   ); 
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ export default {
       backgroundImage: {
         'dark-blue-gradient': 'linear-gradient(180deg, #1B1E27, #333641)',
       },
+      colors: {
+        'primary-color-text': '#EEEEEE',
+        'secundary-color-text': '#D9D9D9',
+        'primary-button' : 'rgba(66,76,92,0.7)',
+      },
     },
   },
   plugins: [],
